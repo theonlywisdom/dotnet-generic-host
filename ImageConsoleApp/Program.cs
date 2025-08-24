1fdd674c8ee07455e13b7f9d1e3855b0d263f474
theonlywisdom <mailbaah@gmail.com>
2025-08-24 14:39:14 +0100

    add command line mappings

--- ImageConsoleApp/Program.cs
+++ ImageConsoleApp/Program.cs
@@ -1,14 +1,24 @@
-﻿using System;
-
-namespace ImageConsoleApp;
+﻿namespace ImageConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var switchMappings = new Dictionary<string, string>
+        {
+            { "-tw", "thumbnail:Width" },
+            { "-tp", "thumbnail:FilePrefix" },
+            { "-mw", "medium:Width" },
+            { "-mp", "medium:FilePrefix" },
+            { "-lw", "large:Width" },
+            { "-lp", "large:FilePrefix" },
+            { "-wt", "watermarkText" },
+            { "-cl", "compressionLevel" }
+        };
+
         IConfigurationRoot configuration = new ConfigurationBuilder()
             .AddJsonFile("config.json")
-            .AddCommandLine(args)
+            .AddCommandLine(args,switchMappings)
             .Build();
 
         Console.WriteLine("***** Process Image *****");
--- ImageConsoleApp/Usings.cs
+++ ImageConsoleApp/Usings.cs
@@ -1,1 +1,3 @@
 ﻿global using Microsoft.Extensions.Configuration;
+global using System;
+global using System.Collections.Generic;
