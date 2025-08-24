31f446bc352290d8506dc6e8e8ccb8de924ca5ee
theonlywisdom <mailbaah@gmail.com>
2025-08-24 14:29:14 +0100

    group config into sections

--- ImageConsoleApp/Program.cs
+++ ImageConsoleApp/Program.cs
@@ -14,12 +14,12 @@ class Program
         Console.WriteLine("***** Process Image *****");
         Console.WriteLine($" Processing: {args[0]}");
 
-        Console.WriteLine($"Thumbnail Width: {configuration["thumbnailWidth"]}");
-        Console.WriteLine($"Thumbnail FilePrefix: {configuration["thumbnailFilePrefix"]}");
-        Console.WriteLine($"Medium Width : {configuration["mediumWidth"]}");
-        Console.WriteLine($"Medium FilePrefix : {configuration["mediumWidthPrefix"]}");
-        Console.WriteLine($"Large Width  : {configuration["largeWidth"]}");
-        Console.WriteLine($"Large FilePrefix  : {configuration["largeWidthPrefix"]}");
+        Console.WriteLine($"Thumbnail Width: {configuration["thumbnail:Width"]}");
+        Console.WriteLine($"Thumbnail FilePrefix: {configuration["thumbnail:FilePrefix"]}");
+        Console.WriteLine($"Medium Width : {configuration["medium:Width"]}");
+        Console.WriteLine($"Medium FilePrefix : {configuration["medium:FilePrefix"]}");
+        Console.WriteLine($"Large Width  : {configuration["large:Width"]}");
+        Console.WriteLine($"Large FilePrefix  : {configuration["large:FilePrefix"]}");
         Console.WriteLine($"Watermark Text : {configuration["watermarkText"]}");
         Console.WriteLine($"Compression Level : {configuration["compressionLevel"]}");
     }
