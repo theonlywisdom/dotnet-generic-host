cc337a2de40d3c91bb8bd0001e17409bf1bbed08
theonlywisdom <mailbaah@gmail.com>
2025-08-24 13:25:24 +0100

    use settings from config.json

--- ImageConsoleApp/Program.cs
+++ ImageConsoleApp/Program.cs
@@ -6,9 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        IConfigurationBuilder
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddJsonFile("config.json");
+
+       IConfigurationRoot configuration = configurationBuilder.Build();
 
         Console.WriteLine("***** Process Image *****");
         Console.WriteLine($" Processing: {args[0]}");
+
+        Console.WriteLine($"Thumbnail Width: {configuration["thumbnailWidth"]}");
+        Console.WriteLine($"Thumbnail FilePrefix: {configuration["thumbnailFilePrefix"]}");
+        Console.WriteLine($"Medium Width : {configuration["mediumWidth"]}");
+        Console.WriteLine($"Medium FilePrefix : {configuration["mediumWidthPrefix"]}");
+        Console.WriteLine($"Large Width  : {configuration["largeWidth"]}");
+        Console.WriteLine($"Large FilePrefix  : {configuration["largeWidthPrefix"]}");
+        Console.WriteLine($"Watermark Text : {configuration["watermarkText"]}");
+        Console.WriteLine($"Compression Level : {configuration["compressionLevel"]}");
     }
 }
