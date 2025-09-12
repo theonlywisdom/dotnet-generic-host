891c7b0ebf3c21589810f1deb2c06ad4f81c8077
theonlywisdom <mailbaah@gmail.com>
2025-09-12 11:04:17 +0100

    fix section configuration for image size config

--- ImageService/Program.cs
+++ ImageService/Program.cs
@@ -9,10 +9,16 @@ builder
         .AddSingleton<IThumbnailProcessor, ThumbnailProcessor>();
     var config = hostContext.Configuration;
 
-    services.Configure<ImageConfig>(config.GetSection(nameof(ImageConfig)));
-    services.Configure<ImageConfig>(ImageSizeConfig.Thumbnail, config.GetSection("ImageConfig:Thumbnail"));
-    services.Configure<ImageConfig>(ImageSizeConfig.Medium, config.GetSection("ImageConfig:Medium"));
-    services.Configure<ImageConfig>(ImageSizeConfig.Large, config.GetSection("ImageConfig:Large"));
+    services.AddOptions<ImageConfig>()
+        .Configure(imageConfig =>
+        {
+            imageConfig.CompressionLevel = 0.99m;
+        })
+        .Bind(config.GetSection(nameof(ImageConfig)));
+
+    services.Configure<ImageSizeConfig>(ImageSizeConfig.Thumbnail, config.GetSection("ImageConfig:Thumbnail"));
+    services.Configure<ImageSizeConfig>(ImageSizeConfig.Medium, config.GetSection("ImageConfig:Medium"));
+    services.Configure<ImageSizeConfig>(ImageSizeConfig.Large, config.GetSection("ImageConfig:Large"));
 });
 
 var host = builder.Build();
